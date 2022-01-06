eadd42f95cc3f48b2eb788770612e5ba9e5ffda7
零式的天空 <0@0skyu.cn>
2022-01-06 17:40:03 +0800

    Update Header.vue

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -2,11 +2,11 @@
   <header class="header">
     <Logo />
 
-    <h2 class="site-title">Color Avatar</h2>
+    <h2 class="site-title">零域色彩头像</h2>
 
     <div class="header-right">
       <a
-        href="https://github.com/Codennnn/vue-color-avatar"
+        href="https://github.com/dstansice/vue-color-avatar"
         target="_blank"
         rel="nofollow noopener noreferrer"
       >
